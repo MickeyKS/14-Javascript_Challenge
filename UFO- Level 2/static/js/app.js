1234b63ee0b96ee214ba462453695aaff6889770
Mickey <68574917+MickeyKS@users.noreply.github.com>
2020-12-01 16:00:42 +1100

    Update Level 2

--- UFO- Level 2/static/js/app.js
+++ UFO- Level 2/static/js/app.js
@@ -1,4 +1,53 @@
-// from data.js
+//From Data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+//Variable Declaration
+var tbody = d3.select("tbody");
+
+//Fill Table
+tableData.forEach((ufo) => {
+    var row = tbody.append("tr");
+    Object.entries(ufo).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+//Select Button
+var button = d3.select("#filter-btn");
+
+//Create Event Handlers 
+button.on("click", runEnter);
+
+//Run Event Handlers
+function runEnter() {
+
+  //Prevent Refresh
+  d3.event.preventDefault();
+
+  //Clear Table Before Search
+  tbody.html("");
+  
+  //Get Raw HTML Node
+  var inputElement = d3.select("#variable");
+
+  //Get Value
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+  console.log(tableData);
+
+  //Filter Data
+  var filteredData = tableData.filter(ufoforday => ufoforday.datetime === inputValue || ufoforday.city === inputValue || ufoforday.state === inputValue || ufoforday.country === inputValue || ufoforday.shape === inputValue);
+                                                                    
+  console.log(filteredData);
+
+  //Fill Table with Filtered Data
+  filteredData.forEach((ufosightings) => {
+    var row = tbody.append("tr");
+    Object.entries(ufosightings).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+  });
+};
